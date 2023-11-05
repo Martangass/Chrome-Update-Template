99e6a63c6b3e6b8aaeddc1d1714c10ac277ef6b5
Marvin Seber <148477227+Martangass@users.noreply.github.com>
2023-11-05 03:04:50 +0100

    Add files via upload

---
+++ German/script.js
@@ -0,0 +1,8 @@
+// Code von Martangass
+// GitHub: https://github.com/Martangass
+
+// NUR FÜR BILDUNGSZWECKE
+
+// Bis jetzt ist das script leer!
+
+// Button atribut = js-download (class in html dokument)
